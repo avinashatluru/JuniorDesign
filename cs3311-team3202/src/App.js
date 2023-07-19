0f389f4dbd20b8136e8015dd5f7061d043280058
Bill <bxiao47@gatech.edu>
2023-07-19 14:54:39 -0400

    Revert "First Commit"
    This reverts commit 92f909d275b7cdfc1dc643f85bf4b47ab25257ba.

--- cs3311-team3202/src/App.js
+++ cs3311-team3202/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Navigate, Routes,} from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Add from "./pages/Add";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />}/>
+        <Route path="/Login" element={<Login />}/>
+        <Route path="/Add" element={<Add />}/>
+        <Route path="*" element={<Navigate to="/"replace={true}/>} />
+      </Routes>
+    </Router>
   );
 }
 
