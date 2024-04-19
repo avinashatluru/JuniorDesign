a05db3c2ea9c414a26392fc49e9bf7fdfa42452c
KingArthurClay <thekingofthebritons@gmail.com>
2024-04-18 20:53:15 -0400

    Added Placeholder UI for filtering the Attendees List

--- client/src/Components/AttendeeDisplay.jsx
+++ client/src/Components/AttendeeDisplay.jsx
@@ -9,6 +9,17 @@ const AttendeeDisplay = ({list, onUpdate}) => {
 	const [checked, setChecked] = useState([]);
 	const [programs, setPrograms] = useState([]);
 	const [currentProgramId, setCurrentProgramId] = useState('');
+	const [currentFilterType, setCurrentFilterType] = useState("");
+	const [currentFilters, setCurrentFilters] = useState([])
+
+	const ageRanges = [	
+		'0-4',
+		'5-9',
+		'10-14',
+		'15-19',
+		'20-24',
+		// Add more ranges as needed
+		];
 
 	// Fetch all programs on component mount
 	useEffect(() => {
@@ -26,51 +37,93 @@ const AttendeeDisplay = ({list, onUpdate}) => {
 	  }, []);
 
 	//Places and removes attendees to/from checked based on whether or not thei checkbox is checked 
-  //Note attendees are passed in formated strings `${attendee._id};${attendee.firstName} ${attendee.lastName}` through e.target.value
-  const handleCheck = (e) => {
-	var x = [...checked]
-	if(e.target.checked) {
-		x = [...checked, e.target.value]
+	//Note attendees are passed in formated strings `${attendee._id};${attendee.firstName} ${attendee.lastName}` through e.target.value
+	const handleCheck = (e) => {
+		var x = [...checked]
+		if(e.target.checked) {
+			x = [...checked, e.target.value]
 
-		//Dirty hack to sort attendees based on ID
-		x = x.sort((a, b) => {
-			let str1 = a.split(";")[0];
-			let str2 = b.split(";")[0];
-			return (str1 < str2 ) ? -1 : ( str1 > str2 ? 1 : 0 )})
-	} else {
-		//Selectively delete the unselected attendees
-		x.splice(checked.indexOf(e.target.value), 1)
+			//Dirty hack to sort attendees based on ID
+			x = x.sort((a, b) => {
+				let str1 = a.split(";")[0];
+				let str2 = b.split(";")[0];
+				return (str1 < str2 ) ? -1 : ( str1 > str2 ? 1 : 0 )})
+		} else {
+			//Selectively delete the unselected attendees
+			x.splice(checked.indexOf(e.target.value), 1)
+		}
+		setChecked(x)
 	}
-	setChecked(x)
-  }
 
-  const clearMultiSelection = () => {
-	Array.from(document.getElementsByClassName("attendeeCheck")).forEach(element => {
-		element.checked = false;
-	});
-	setChecked([]);
-  }
+	const addSelectedUsers = (e) => {
+		console.log("I'm not impletemented yet!")
+	}
 
-  const handleProgramSelect = (e) => {
-    const selectedProgramId = e.target.value;
-    setCurrentProgramId(selectedProgramId);
-  
-    
-    }
+	const removeSelectedUsers = (e) => {
+		console.log("I'm not impletemented yet!")
+	}
+
+	const clearMultiSelection = () => {
+		Array.from(document.getElementsByClassName("attendeeCheck")).forEach(element => {
+			element.checked = false;
+		});
+		setChecked([]);
+	}
+
+	const handleProgramSelect = (e) => {
+		const selectedProgramId = e.target.value;
+		setCurrentProgramId(selectedProgramId);
+	}
+
+	const handleFilterTypeSelect = (e) => {
+		setCurrentFilterType(e.target.value);
+	}
+
+	const createFilter = (e) => {
+		console.log("I'm not impletemented yet!")
+	}
 
 	return (
 		<div className="container horizontal" id="rosterContainer">
 			<div style={{width: "33%"}}>
 				<div className="container vertical">
 					<h4>Filters</h4>
-					<div><p>Filter by:</p>
-						<select>
-							<option>Program</option>
-							<option>Age Range</option>
-							<option>Name</option>
+					<div style={{marginBottom: "4pt"}}><p>Filter by:</p>
+						<select onChange={handleFilterTypeSelect}t>
+							<option value={""}>Select an Option</option>
+							<option value={"Program"}>Program</option>
+							<option value={"Age Range"}>Age Range</option>
+							<option value={"Name"}>Name</option>
 						</select>
 					</div>
-					
+					<div>
+						{currentFilterType == "Program" && <>
+							<select>
+								<option value="">Select a program</option>
+								{programs.map(program => (
+									<option key={program._id} value={program._id}>{program.name}</option>
+								))}
+							</select>
+						</>}
+						{currentFilterType == "Age Range" && <>
+							<select>
+								<option value="">Select an Age Range</option>
+								{ageRanges.map((ageRange) => (
+									<option value={ageRange}>{ageRange}</option>
+								))}
+							</select>
+						</>}
+						{currentFilterType == "Name" && <>
+								<textarea/>
+						</>}
+						{currentFilterType != "" && <>
+							<button onClick={createFilter}>Create Filter</button>
+						</>}
+					</div>
+					<div>
+						<p>No Current Filters</p>
+						<button className="delete">Remove Selected Filters</button>
+					</div>
 				</div>
 			</div>
 			<div style={{width: "33%"}}>
@@ -83,20 +136,23 @@ const AttendeeDisplay = ({list, onUpdate}) => {
 				<div><input type="checkbox" value={"Multi-Selection"} onChange={() => {setMultiSelect(!multiSelectEnabled)}}/><h4 className={`${multiSelectEnabled ? "active" : ""}`}>Multi-Selection</h4></div>
 				{multiSelectEnabled ? <><h4>Selected Attendees</h4>
 				{checked.map((item, index) => ( 
-				/** 
-				 * Note attendees are passed in formated strings `${attendee._id};${attendee.firstName} ${attendee.lastName}`
-				 * splitting ensures only the name is displayed
-				*/
-       			<div key={index}>
-        			<span>{item.split(";")[1]}</span>
-       			</div>))}
-				<div className="container vertical"><select onChange={handleProgramSelect} value={currentProgramId}>
-					<option value="">Select a program</option>
-					{programs.map(program => (
-						<option key={program._id} value={program._id}>{program.name}</option>
-					))}
+					/** 
+					 * Note attendees are passed in formated strings `${attendee._id};${attendee.firstName} ${attendee.lastName}`
+					 * splitting ensures only the name is displayed
+					*/
+					<div key={index}>
+						<span>{item.split(";")[1]}</span>
+					</div>))}
+					<div className="container vertical"><select onChange={handleProgramSelect} value={currentProgramId}>
+						<option value="">Select a program</option>
+						{programs.map(program => (
+							<option key={program._id} value={program._id}>{program.name}</option>
+						))}
 					</select>
-				<input type="button" value={"Add to Program"}/><input type="button" value={"Remove from Program"}/><input style={{color: "red"}} type="button" value={"Clear Selection"} onClick={clearMultiSelection}/></div></> : null}
+					<button onClick={addSelectedUsers}>Add to Program</button>
+					<button onClick={removeSelectedUsers}>Remove from Program</button>
+					<button className="delete" onClick={clearMultiSelection}>Clear Selection</button>
+				</div></> : null}
 			</div>
 		</div>
 	);
